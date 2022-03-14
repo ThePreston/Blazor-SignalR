91ea187ca25a38484b496ba2f9078c882990f00d
Preston Hale <prestonhale@microsoft.com>
2022-03-14 11:18:20 -0500

    LogicApp Added

--- BlazorSignalRApp/Server/Hubs/ChatHub.cs
+++ BlazorSignalRApp/Server/Hubs/ChatHub.cs
@@ -10,6 +10,12 @@ namespace BlazorSignalRApp.Server.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
+
+            //Todo Call Logic App with
+            //Listen to Event Grid,
+            //messageclient
+
+
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
